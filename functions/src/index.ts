8d101aaeac473cdb6bcc191c36fe91d17175766c
F88 <msn088g@gmail.com>
2020-05-19 18:27:13 +0900

    test api server on firebase

---
+++ functions/src/index.ts
@@ -0,0 +1,16 @@
+import * as functions from 'firebase-functions'
+
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   response.send('Hello from Firebase!')
+// })
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+// exports.api = functions.https.onRequest(server)
+export const api = functions.https.onRequest(server)
